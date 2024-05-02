764ea8cf7b7981614d9961f1451b397431ae6be7
Arnau Taberner García <40540187+arnautaga@users.noreply.github.com>
2024-05-02 19:30:16 +0200

    add

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class ARQRED:
         self.dir_socket_servidor = ("158.42.188.200", 64010)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def iniciar_sesion(self, username, password):
+    def iniciar_sesion(self, username, password, gui):
         ip = self.obtener_ip()
         if ip:
             try:
@@ -21,6 +21,7 @@ class ARQRED:
                         data = self.sock.recv(1024).decode()
                         if "200" in data:
                             print("Inicio de sesión exitoso.")
+                            gui.ventana_principal(True)
                         else:
                             print("Error: ", data)
                     else:
@@ -55,6 +56,7 @@ class GUI:
     def __init__(self):
         self.root = gz.App(title="APP")
         self.arqred = ARQRED()
+        self.logeado = False  # Estado de inicio de sesión
         self.abrir_ventana_bienvenida()
 
     def abrir_ventana_bienvenida(self):
@@ -75,6 +77,7 @@ class GUI:
     def acceder_como_invitado(self):
         # Lógica para acceder como invitado
         print("Accediendo como invitado")
+        self.ventana_principal(False)  # Mostrar ventana principal como invitado
 
     def ventana_iniciar_sesion(self):
         # Crear ventana para iniciar sesión
@@ -92,8 +95,19 @@ class GUI:
         password.tk.pack(pady=5)
 
         # Boton para entrar en la app
-        boton_iniciar_sesion = gz.PushButton(ventana_login, text="Iniciar sesión", command=lambda: self.arqred.iniciar_sesion(usuario.value, password.value))
+        boton_iniciar_sesion = gz.PushButton(ventana_login, text="Iniciar sesión", command=lambda: self.arqred.iniciar_sesion(usuario.value, password.value, self))
         boton_iniciar_sesion.tk.pack(pady=5)
+
+    def ventana_principal(self, loged=False):
+        self.logeado = loged  # Actualizar el estado de inicio de sesión
+        ventana_principal = gz.Window(self.root, title="APP")
+        if loged:
+            # Mostrar opciones para usuario logeado
+            label = gz.Text(ventana_principal, text="¡Bienvenido!")
+        else:
+            # Mostrar opciones para usuario invitado
+            label = gz.Text(ventana_principal, text="¡Bienvenido, invitado!")
+        label.tk.pack()
 
 if __name__ == "__main__":
     gui = GUI()
